c55acbffd1357bfd493737f3d7c1826beb58797c
raviq <rafik.hadfi@gmail.com>
2018-11-16 22:37:47 +1100

    initial project version

---
+++ README.md
@@ -0,0 +1,17 @@
+# Portfolio website
+
+My very basic portfolio in html.
+
+## Usage
+
+- Edit the `sidemenu` div entries in `index.html` and create the corresponding pages in `include/`.
+- Test by adding the site to `public/`, running [http-server](https://github.com/indexzero/http-server) using `http-server -p 8000`, and accessing `http://localhost:8000/`.
+
+
+## Licence
+This software was developed in the hope that it would be of some use to the agent research community, and is freely available for redistribution and/or modification under the terms of the GNU General Public Licence. It is distributed WITHOUT WARRANTY; without even the implied warranty of merchantability or fitness for a particular purpose. See the [GNU General Public License](https://github.com/raviq/Genon/blob/master/LICENCE.md) for more details. 
+
+Comments, feedback and ideas are welcome. If you find bugs or omissions in the code, please do not hesitate to submit an issue or open a pull request with a fix.
+
+Copyright (c) 2018 Rafik Hadfi, rafik.hadfi@gmail.com
+
---
+++ js/scripts.js
@@ -0,0 +1,68 @@
+////////////////////////////////////////////////////////////////
+// Title: JS routines
+// Version: 0.9
+// Last change: Nov 2018
+// Author: Rafik Hadfi (rafik.hadfi@gmail.com)
+// Repository: https://github.com/raviq
+////////////////////////////////////////////////////////////////
+ 
+// Loading menu and pages
+$(document).ready(function ()
+{	
+	// Loading side menu
+    $('.sidemenu ul li:first').addClass('active');
+    $('.tab-content:not(:first)').hide();
+    $('.sidemenu ul li a').click(function (event) 
+    {
+        event.preventDefault();
+        var content = $(this).attr('href');
+        $(this).parent().addClass('active');
+        $(this).parent().siblings().removeClass('active');
+        $(content).show();
+        $(content).siblings('.tab-content').hide();
+    });
+
+    // Loading pages and inclusion in index page
+	var pages = [	"welcome",
+					"publications", 
+					"skills", 
+					"education", 
+					"experience", 
+					"social", 
+					"resources",
+					"activities"
+				];
+			
+	for (var i in pages) 
+		$('#section-' + pages[i]).load("include/" + pages[i] + ".html");
+
+});
+
+// Email obfuscation through random encryption.
+// source: http://www.jottings.com/obfuscator/
+function obfuscate()
+{
+	// TODO Change with your email-key:
+	coded = "EV2ba.PVl2b@jvVbz.oNv";
+	key   = "5xW4wamD1g6FCycfOdYhiJGALXVv09juQrto2IlRPbeps7kq3Z8nHUESNMKzTB";
+	for (link = "", i = 0 ; i < coded.length ; i++) 
+	 	link += key.indexOf(coded.charAt(i)) == -1 ? 
+	 			  coded.charAt(i) :
+	 			     key.charAt((key.indexOf(coded.charAt(i)) - coded.length + key.length) % key.length);
+	return link;
+}
+
+// Toggle between hiding and showing kth Details.
+function seeDetails(k) 
+{
+    var x = document.getElementById("Details" + k);
+    x.style.display = (x.style.display === "none") ? "block" : "none";
+}
+
+
+
+
+
+
+
+
